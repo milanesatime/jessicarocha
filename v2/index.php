d4e51735189c3423074317e25da5de79f587ab53
Jessica Rocha Lim <mail@jessicarocha.com>
2014-07-08 22:55:23 -0400

    less lesshat variables mixins etc nonsense hello world

--- v2/index.php
+++ v2/index.php
@@ -1,108 +1,112 @@
-<?php include_once("includes/header.php"); ?>
- 
-<!-- Modal -->
-<div id="hireme" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="hireme" aria-hidden="true">
-	<div class="modal-header">
-		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-		<h3>Hire Me!</h3>
-	</div>
-	<div class="modal-body">
-		<p>Interested in working with me?  Tell me a little about yourself and your goals for your project and I'll get back to you as soon as I can.</p>
-    	<?php
-		if (isset($_REQUEST['email']))
-		//if "email" is filled out, send email
-		  {
-		  //send email
-		  $name = $_REQUEST['name'] ;
-		  $email = $_REQUEST['email'] ;
-		  $subject = $_REQUEST['subject'] ;
-		  $message = $_REQUEST['message'] ;
-		  mail("mail@jessicarocha.com", $subject,
-		  $message, "From:" . $email);
-		  echo "Thank you!";
-		  }
-		else
-		//if "email" is not filled out, display the form
-		  {
-		  echo "<form method='post' action='index.php'>
-		  </form>
-		  <form class='form' id='hire-form'>
-		      <div class='control-group'>
-		        <label class='control-label'>Name</label>
-		        <div class='controls'>
-		          <input type='text' name='name' placeholder='Name'>
-		        </div>
-		      </div>
-		      <div class='control-group'>
-		        <label class='control-label'>Email</label>
-		        <div class='controls'>
-		          <input type='text' name='email' placeholder='Email'>
-		        </div>
-		      </div>
-		      <div class='control-group'>
-		        <div class='controls'>
-		          <textarea rows='3' name='message' placeholder='Tell me a little about your project.'></textarea>
-		        </div>
-		      </div>
-		      <div class='control-group'>
-		        <div class='controls'>
-		          <input type='hidden' name='subject' value='Message from JessicaRocha.com'>
-		          <button type='submit' class='btn'>Submit</button>
-		        </div>
-		      </div>
-		    </form>";
-		  }
-		?>
-	</div>
-	<div class="modal-footer"></div>
-</div>
-<!--accordion items-->
 
-<div class="accordion" id="proj">
-  <div class="accordion-group">
-    <div id="main-proj" class="row-fluid accordion-body collapse in">
-      <?php include_once("projects/featured.php"); ?>
-      <div class="span5" id="about-me">
-        <h3 class="title">About Me</h3>
-        <img src="assets/images/about-me.png" alt="about-me" width="169" height="169" class="img-polaroid pull-left">
-        <p>My aim as a developer is to create aesthetically pleasing, cross-browser compatible, valid code. All of my sites are W3C validated HTML5 and search engine optimized. My goal is to ensure that my client is happy with my work which is completed on time or ahead of schedule.</p>
-        <p>I'm an NYC resident and Hunter College graduate with a BA in Media Studies. I've been coding since the good old days when <code>&#60;blink&#62;</code> tags were acceptable and there was no such thing as CSS. I always strive to keep up to date with new standards and practices and enjoy challenging myself.</p>
-        <p><strong>Contact me: </strong><br>
-          <img src="assets/images/email.png" alt="email" width="24" height="24"><a href="mailto:mail@jessicarocha">mail@jessicarocha.com</a><br>
-          <a href="http://www.linkedin.com/in/jessrocha/"><img src="assets/images/linkedin.png" alt="linkedin" width="24" height="24">@jessrocha</a><br>
-          <img src="assets/images/skype.png" alt="skype" width="24" height="24"><a href="skype:hijessrocha?call">@hijessrocha</a></p>
-        <p><strong>Download my resumé: </strong><a href="assets/docs/jessicarocha_resume.pdf"><img src="assets/images/pdficon_large.png" alt="pdficon_large" width="32" height="32"></a></p>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Jessica Rocha Lim is an NYC based Front End Developer with experience in Javascript/jQuery, CSS/CSS3, HTML/HTML5, Wordpress and Drupal. ">
+    <meta name="author" content="Jessica Rocha Lim">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Jessicarocha.com</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+      <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">
+            <img src="assets/rocha-brand-text.png" alt="rocha-brand-text" width="142" height="24">
+          </a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#hireme">Hire Me!</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Contact <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="https://github.com/milanesatime/jessicarocha">GitHub</a></li>
+                <li><a href="https://www.linkedin.com/in/jessrocha">LinkedIn</a></li>
+                <li><a href="https://www.drupal.org/u/jessicamilanesa">Drupal.org</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
       </div>
-      <!--.span--> 
     </div>
-    <!--.row-fluid-->
-  </div>
-  <!--.accordion group-->
-  <h3 class="title">Recent Work</h3>
-  <?php include_once("projects/proj-1.php"); ?>
-  <?php include_once("projects/proj-2.php"); ?>
-  <?php include_once("projects/proj-3.php"); ?>
-</div>
-<!--end accordion items-->
-<div class="row-fluid" id="previews">
-  <div class="span4">
-    <h4>Reality Teen</h4>
-    <p><img src="assets/images/projects/reality-teen/rt-preview.png" class="img-polaroid" alt="Reality Teen"></p>
-    <p><a class="btn accordion-toggle" data-toggle="collapse" data-parent="#proj" href="#proj-1">More &raquo;</a></p>
-  </div>
-  <!--/span-->
-  <div class="span4">
-    <h4>Natalia Nakazawa</h4>
-    <p><img src="assets/images/projects/natalia-nakazawa/nn-preview.png" class="img-polaroid" alt="Natalia Nakazawa"></p>
-    <p><a class="btn accordion-toggle" data-toggle="collapse" data-parent="#proj" href="#proj-2">More &raquo;</a></p>
-  </div>
-  <!--/span-->
-  <div class="span4">
-    <h4>Fred Harper</h4>
-    <p><img src="assets/images/projects/fred-harper/fh-preview.png" class="img-polaroid" alt="Fred Harper"></p>
-    <p><a class="btn accordion-toggle" data-toggle="collapse" data-parent="#proj" href="#proj-3">More &raquo;</a></p>
-  </div>
-  <!--/span--> 
-</div>
-<!--.row-->
-<?php include_once("includes/footer.php"); ?>
+    
+    <section class="wrapper">
+      <article class="container">
+
+        <div class="page-header">
+          <h1>I am...</h1>
+          <p class="lead">a New York City based web developer who's made her home in Astoria. Besides coding and nerding out
+          I also love sriracha, alfajores, authors of the old cranky Russian and/or American variety, sunburn, and Swedish hip hop.</p>
+        </div>
+      </article>
+    </section>
+
+    <section class="wrapper">
+      <article class="container">
+        <h3>Technologies and frameworks I adore</h3>
+        <div class="row">
+          <div class="col-md-4">LESS</div>
+          <div class="col-md-4">Git &amp; Github</div>
+          <div class="col-md-4">Bootstrap</div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4">HTML &amp; HTML5</div>
+          <div class="col-md-4">CSS &amp; CSS3</div>
+          <div class="col-md-4">Javascript &amp; jQuery</div>
+        </div>
+      </article>
+    </section>
+
+    <section class="wrapper">
+      <article class="container">
+
+        <h3>Other technologies that <s>I adore</s> pay the bills</h3>
+        <p>Just being <strong>honest</strong>.</p>
+        <div class="row">
+          <div class="col-md-4">Drupal</div>
+          <div class="col-md-4">Wordpress</div>
+          <div class="col-md-4">PHP</div>
+        </div>
+      </article>
+    </section>
+
+      <hr>
+
+    </div> <!-- /container -->
+
+        <div class="footer">
+      <div class="container">
+        <p class="text-muted">&copy; <?php echo date("Y") ?> Jessica Rocha Lim</p>
+      </div>
+    </div>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="bootstrap/js/dropdown.js"></script>
+
+  </body>
+</html>
